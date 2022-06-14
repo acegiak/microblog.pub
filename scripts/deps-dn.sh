2c4f437270a56c9bc98254f8543bdf1d946520ac
Michael Penhallegon <mike@hematite.tech>
2022-06-13 18:32:02 -0700

    update with comment and set +x and reorganize

--- scripts/deps-dn.sh
+++ scripts/deps-dn.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set +x
+
 podman pod kill web
 podman pod kill mongo
 podman pod kill poussetaches
--- scripts/deps-up.sh
+++ scripts/deps-up.sh
@@ -1,22 +1,9 @@
-if [ "$(podman network exists microblogpub-network)" == "1" ]; then
-    podman network create microblogpub-network
-fi
+#!/bin/bash
 
-podman pod create \
-    --network=microblogpub-network \
-    -p 5005:5005 \
-    -n web
+# create network if needed
+podman network create microblogpub-network
 
-podman run -dt --pod web \
-    -e MICROBLOGPUB_MONGODB_HOST=mongo:27017 \
-    -e MICROBLOGPUB_INTERNAL_HOST=http://microblogpub:5005 \
-    -e MICROBLOGPUB_POUSSETACHES_HOST=http://poussetaches:7991 \
-    -e POUSSETACHES_AUTH_KEY=1234 \
-    -e COMPOSE_PROJECT_NAME=microblogpub \
-    -v ./config:/app/config:z \
-    -v ./static:/app/static:z \
-    localhost/microblogpub:latest
-
+# create mongo server
 podman pod create \
     --network=microblogpub-network \
     -n mongo
@@ -25,6 +12,7 @@ podman run -dt --pod mongo \
     -v ./data/mongodb:/data/db:z \
     docker.io/mongo:3
 
+# create poussetaches server
 podman pod create \
     --network=microblogpub-network \
     -n poussetaches
@@ -33,3 +21,19 @@ podman run -dt --pod poussetaches \
     -v ./data/poussetaches:/app/poussetaches_data:z \
     -e POUSSETACHES_AUTH_KEY=1234 \
     docker.io/poussetaches/poussetaches:latest
+
+# create microblogpub app server
+podman pod create \
+    --network=microblogpub-network \
+    -p 5005:5005 \
+    -n web
+
+podman run -dt --pod web \
+    -e MICROBLOGPUB_MONGODB_HOST=mongo.dns.podman:27017 \
+    -e MICROBLOGPUB_INTERNAL_HOST=http://microblogpub.dns.podman:5005 \
+    -e MICROBLOGPUB_POUSSETACHES_HOST=http://poussetaches.dns.podman:7991 \
+    -e POUSSETACHES_AUTH_KEY=1234 \
+    -e COMPOSE_PROJECT_NAME=microblogpub \
+    -v ./config:/app/config:z \
+    -v ./static:/app/static:z \
+    localhost/microblogpub:latest
