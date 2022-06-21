722071e2019a61a64a37b57308ff51a22cee4233
Michael Penhallegon <mike@hematite.tech>
2022-06-20 18:09:11 -0700

    changed package name and updated run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # TODO: create migrations init
 python -c "import logging; logging.basicConfig(level=logging.DEBUG); from core import migrations; migrations.perform()"
 python -c "from core import indexes; indexes.create_indexes()"
-python startup.py
+python -m microblogpub.startup
 (sleep 5 && curl -X POST -u :$POUSETACHES_AUTH_KEY $MICROBLOGPUB_POUSSETACHES_HOST/resume)&
-gunicorn --workers 3 -b 0.0.0.0:5005 --log-level debug app:app
+gunicorn --workers 3 -b 0.0.0.0:5005 --log-level debug microblogpub.app:app
