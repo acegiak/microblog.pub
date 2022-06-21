7867473b04c13cbb594cb7e3200e4940ceb9b4bf
Michael Penhallegon <mike@hematite.tech>
2022-06-20 22:59:35 -0700

    added dependency start up script that is podman/docker-compose agnostic

---
+++ scripts/deps-dn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set +x
+
+if command -v "podman"; then
+    source scripts/podman-deps-dn.sh
+else
+    docker-compose dn
+fi
---
+++ scripts/deps-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set +x
+
+if command -v "podman"; then
+    source scripts/podman-deps-up.sh
+else
+   docker-compose -f scripts/docker-compose.yml 
+fi
