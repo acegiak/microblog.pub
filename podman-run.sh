c66e7e1647fdacf7868d3d32b004258313cc9b4f
Michael Penhallegon <mike@hematite.tech>
2022-06-20 18:08:19 -0700

    remove old files and added conditional check for network and pods

---
+++ podman-run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set +x
+
+
+source ./scripts/deps-up.sh
+
+podman pod create \
+    --network=microblogpub-network \
+    -n web \
+    -p 5005:5005
+
+# start container with microblogpub prod image
+podman run -dt --pod web \
+    -e MICROBLOGPUB_MONGODB_HOST=mongo.dns.podman:27017 \
+    -e MICROBLOGPUB_INTERNAL_HOST=http://microblogpub.dns.podman:5005 \
+    -e MICROBLOGPUB_POUSSETACHES_HOST=http://poussetaches.dns.podman:7991 \
+    -e POUSSETACHES_AUTH_KEY=1234 \
+    -e COMPOSE_PROJECT_NAME=microblogpub \
+    -v ./config:/app/config:z \
+    -v ./static:/app/static:z \
+	-v ./src/templates:/app/templates \
+	-v ./src/sass:/app/sass \
+    localhost/microblogpub:latest
