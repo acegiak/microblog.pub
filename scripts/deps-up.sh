281ceda580007bd5bffcd5adb5edcab7ae48c7de
Michael Penhallegon <mike@hematite.tech>
2022-06-20 22:59:55 -0700

    Added in docker-compose from old commit

--- scripts/deps-up.sh
+++ scripts/deps-up.sh
@@ -5,5 +5,5 @@ set +x
 if command -v "podman"; then
     source scripts/podman-deps-up.sh
 else
-   docker-compose -f scripts/docker-compose.yml 
+   docker-compose -f scripts/docker-compose.yml up 
 fi
